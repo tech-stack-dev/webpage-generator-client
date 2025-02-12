432aa9ad21b8e9cf57582ace13110032f186dcb0
Andrii Halai <andrii.halai@tech-stack.io>
2025-02-12 19:43:56 +0200

    Update

--- src/routing/Router.tsx
+++ src/routing/Router.tsx
@@ -1,18 +1,18 @@
 import { Route } from 'react-router';
-import { BrowserRouter, Routes } from 'react-router';
+import { HashRouter, Routes } from 'react-router';
 import { App } from '../App';
 import { OldGenerator } from '@pages/oldGenerator/OldGenerator';
 import { ContentGeneratorPage } from '@pages/contentGenerator/ContentGeneratorPage';
 
 export const AppRouter = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<ContentGeneratorPage />} />
         </Route>
         <Route path="old-generator" element={<OldGenerator />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
