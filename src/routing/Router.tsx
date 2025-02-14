7839e5241689ee24b4a07942bbcca48b4577e113
Andrii Halai <andrii.halai@tech-stack.io>
2025-02-14 16:07:40 +0200

    Replace hash router with browser router

--- src/routing/Router.tsx
+++ src/routing/Router.tsx
@@ -1,18 +1,17 @@
-import { Route } from 'react-router';
-import { HashRouter, Routes } from 'react-router';
+import { BrowserRouter, Route, Routes } from 'react-router';
 import { App } from '../App';
 import { OldGenerator } from '@pages/oldGenerator/OldGenerator';
 import { ContentGeneratorPage } from '@pages/contentGenerator/ContentGeneratorPage';
 
 export const AppRouter = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<ContentGeneratorPage />} />
         </Route>
         <Route path="old-generator" element={<OldGenerator />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
