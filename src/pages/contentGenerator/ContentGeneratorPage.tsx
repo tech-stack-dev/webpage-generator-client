88e91ff508174caa79cbe8b0d026ec18665a93d0
ValeriiRuchko <valerii.ruchko@tech-stack.io>
2025-04-23 14:59:13 +0200

    TSWEB-1965. Remove Main Content & Hero section elements. Show %breadcrumb conditionally.Automate saving to Airtable.

--- src/pages/contentGenerator/ContentGeneratorPage.tsx
+++ src/pages/contentGenerator/ContentGeneratorPage.tsx
@@ -1,53 +1,11 @@
 import { GeneratePageForm } from './components/GeneratePageForm';
 import styles from './ContentGeneratorPage.module.css';
-import { useAppSelector } from '@/store/hooks';
-import { SaveToAirtableRequest } from '@/api/contentGeneratorApi/types';
-import { useSaveToAirtableMutation } from '@/api/contentGeneratorApi/contentGeneratorApi';
-import toast from 'react-hot-toast';
-import { OutputField } from './components/OutputField';
 
 export const ContentGeneratorPage = () => {
-  const [saveToAirtable] = useSaveToAirtableMutation();
-  const generatedMainContent = useAppSelector(
-    (state) => state.generatedPageSlice.mainContent
-  );
-  const generatedHeroContent = useAppSelector(
-    (state) => state.generatedPageSlice.heroContent
-  );
-  const generatedPage = useAppSelector((state) => state.generatedPageSlice);
-
-  const onClick = async () => {
-    const request: SaveToAirtableRequest = {
-      name: generatedPage.name,
-      breadcrumb: generatedPage.breadcrumb,
-      heroContent: generatedPage.heroContent,
-      heroTitle: generatedPage.heroTitle,
-      mainContent: generatedPage.mainContent,
-      metaDescription: generatedPage.metaDescription,
-      metaTitle: generatedPage.metaTitle,
-      slug: generatedPage.slug,
-    };
-
-    try {
-      await saveToAirtable(request).unwrap();
-      toast.success('Updated airtable', {
-        duration: 3000,
-      });
-    } catch (error) {
-      toast.error('An error occurred! Try again later', {
-        duration: 3000,
-      });
-    }
-  };
-
   return (
     <div className={styles.container}>
       <div className={styles.formWrapper}>
         <GeneratePageForm />
-      </div>
-      <div className={styles.outputsWrapper}>
-        <OutputField title="Main content" text={generatedMainContent} />
-        <OutputField title="Hero section" text={generatedHeroContent} />
       </div>
       <div
         style={{
@@ -56,17 +14,7 @@ export const ContentGeneratorPage = () => {
           alignItems: 'center',
           marginBottom: '1rem',
         }}
-      >
-        <button
-          onClick={onClick}
-          style={{
-            height: '50px',
-            padding: '0.5rem',
-          }}
-        >
-          Save to Airtable
-        </button>
-      </div>
+      ></div>
     </div>
   );
 };
--- src/pages/contentGenerator/components/OutputField/OutputField.tsx
+++ src/pages/contentGenerator/components/OutputField/OutputField.tsx
@@ -6,13 +6,13 @@ interface IOutputFieldProps {
   text: string;
 }
 
+/**
+ * Component that was previosly used for previews of generated content from AI
+ * Not deleted for now in case of need for similar again in the future
+ */
 export const OutputField: FC<IOutputFieldProps> = ({ title, text }) => {
   return (
-    <div
-      style={{
-        width: '100%',
-      }}
-    >
+    <div style={{ width: '100%' }}>
       {title && <p className={styles.titleStyle}>{title}</p>}
       <div className={styles.text}>{text}</div>
       <p className={styles.charactersCount}>Characters count: {text.length}</p>
